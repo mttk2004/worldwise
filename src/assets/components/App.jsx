c4f5db0859c8e85723409ccc8a72405d71e2c5a7
= <=>
2024-08-08 14:39:50 +0700

    v1.0: first commit

---
+++ src/assets/components/App.jsx
@@ -0,0 +1,14 @@
+/**
+ *  Project: worldwise
+ *  File: App.jsx
+ *  Created: 2:35 CH, 08/08/2024
+ *  Author: Mai Tran Tuan Kiet
+ *  "Family is where life begins and love never ends."
+ */
+
+
+function App() {
+	return <div>Hello</div>;
+}
+
+export default App;
